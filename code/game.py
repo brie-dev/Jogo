48d9115024581fedbd082dac70ec5c0e79484e73
Gabrielle <4848903@alunouninter.com>
2025-07-25 03:06:03 -0300

    Background image

--- code/game.py
+++ code/game.py
@@ -1,17 +1,15 @@
 import pygame
 
+from code.const import WIN_WIDTH, WIN_HEIGHT
 from code.menu import Menu
 
 
 class Game:
-    # CRIANDO A TELA
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
-    #INICIANDO O JOGO
     def run(self):
-        #MANTENDO ELA ABERTA
         while True:
             menu = Menu(self.window)
             menu.run()
@@ -19,6 +17,6 @@ class Game:
 
             # Check for all events
             # for event in pygame.event.get():
-            #   if event.type == pygame.QUIT:
-            #       pygame.quit()  # Close window
-            #        quit()  # End pygame
+            #     if event.type == pygame.QUIT:
+            #         pygame.quit()  # Close window
+            #         quit()  # End pygame
