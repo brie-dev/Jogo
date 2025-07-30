902f073e455090ceb5beb0d51cd7d6e2682624fe
Gabrielle <4848903@alunouninter.com>
2025-07-30 01:50:52 -0300

    Creating mediator pattern. Enemies destruction

--- code/const.py
+++ code/const.py
@@ -18,6 +18,17 @@ ENTITY_SPEED = {
     'Enemy2': 1,
 }
 
+ENTITY_HEALTH = {
+    'Level1Bg0': 999,
+    'Level1Bg1': 999,
+    'Level1Bg2': 999,
+    'Level1Bg3': 999,
+    'Player1': 300,
+    'Player2': 300,
+    'Enemy1': 50,
+    'Enemy2': 60,
+}
+
 SPAWN_TIME = 4000
 
 PLAYER_KEY_LEFT = {'Player1': pygame.K_LEFT,
